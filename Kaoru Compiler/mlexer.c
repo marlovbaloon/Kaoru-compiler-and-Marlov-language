bd76a0d624a4f56551aa450fc888b6cb6c51a03e
Marlov <marlovbaloon@gmail.com>
2026-08-27 15:15:26 +0700

    fixed lexical

--- Kaoru Compiler/mlexer.c
+++ Kaoru Compiler/mlexer.c
@@ -1,4 +1,4 @@
-//mlexer.c
+// mlexer.c
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
@@ -20,7 +20,7 @@ Token next_token(FILE *file) {
 
         tok.line = current_line;
 
-        /* 1. Handle Symbol Prefix @ */
+        /* 1. Handle Symbol Prefix @ Directives */
         if (c == '@') {
             char buffer[32] = "@";
             int idx = 1;
@@ -44,6 +44,8 @@ Token next_token(FILE *file) {
             else if (strcmp(buffer, "@tbl") == 0) tok.type = TOKEN_AT_TBL;
             else if (strcmp(buffer, "@print") == 0) tok.type = TOKEN_AT_PRINT;
             else if (strcmp(buffer, "@debug") == 0) tok.type = TOKEN_AT_DEBUG;
+            else if (strcmp(buffer, "@exit") == 0) tok.type = TOKEN_AT_EXIT;
+            else if (strcmp(buffer, "@panic") == 0) tok.type = TOKEN_AT_PANIC;
             else {
                 printf("Kaoru Syntax Error (Line %u): Unknown @ directive %s\n", current_line, buffer);
                 tok.type = TOKEN_EOF;
@@ -55,23 +57,23 @@ Token next_token(FILE *file) {
         if (c == '"') {
             int idx = 0;
             while ((c = fgetc(file)) != EOF && c != '"') {
-                if (c == '\\') { // Escape Character Handling
-                int next = fgetc(file);
-                if (next == 'n') c = '\n';
-                else if (next == 't') c = '\t';
-                else if (next == '"') c = '"';
-                else if (next == '\\') c = '\\';
-                else ungetc(next, file);
+                if (c == '\\') {
+                    int next = fgetc(file);
+                    if (next == 'n') c = '\n';
+                    else if (next == 't') c = '\t';
+                    else if (next == '"') c = '"';
+                    else if (next == '\\') c = '\\';
+                    else ungetc(next, file);
+                }
+                if (c == '\n') current_line++;
+                if (idx < (int)sizeof(tok.value) - 1) {
+                    tok.value[idx++] = (char)c;
+                }
             }
-            if (c == '\n') current_line++;
-            if (idx < (int)sizeof(tok.value) - 1) {
-            tok.value[idx++] = (char)c;
+            tok.value[idx] = '\0';
+            tok.type = TOKEN_STRING_LIT;
+            return tok;
         }
-    }
-    tok.value[idx] = '\0';
-    tok.type = TOKEN_STRING_LIT;
-    return tok;
-    }
 
         /* 3. Numbers */
         if (isdigit(c)) {
@@ -99,23 +101,41 @@ Token next_token(FILE *file) {
             if (strcmp(tok.value, "if") == 0) tok.type = TOKEN_IF;
             else if (strcmp(tok.value, "else") == 0) tok.type = TOKEN_ELSE;
             else if (strcmp(tok.value, "while") == 0) tok.type = TOKEN_WHILE;
+            else if (strcmp(tok.value, "for") == 0) tok.type = TOKEN_FOR;
             else if (strcmp(tok.value, "return") == 0) tok.type = TOKEN_RETURN;
             else tok.type = TOKEN_IDENTIFIER;
 
             return tok;
         }
 
-        /* 5. Delimiters & Single/Double Operators */
+        /* 5. Delimiters & Structural Tokens */
         if (c == '{') { tok.type = TOKEN_LBRACE; strcpy(tok.value, "{"); return tok; }
         if (c == '}') { tok.type = TOKEN_RBRACE; strcpy(tok.value, "}"); return tok; }
         if (c == '(') { tok.type = TOKEN_LPAREN; strcpy(tok.value, "("); return tok; }
         if (c == ')') { tok.type = TOKEN_RPAREN; strcpy(tok.value, ")"); return tok; }
+        if (c == '[') { tok.type = TOKEN_LBRACKET; strcpy(tok.value, "["); return tok; }
+        if (c == ']') { tok.type = TOKEN_RBRACKET; strcpy(tok.value, "]"); return tok; }
         if (c == ';') { tok.type = TOKEN_SEMICOLON; strcpy(tok.value, ";"); return tok; }
+        if (c == ',') { tok.type = TOKEN_COMMA; strcpy(tok.value, ","); return tok; }
+        if (c == '.') { tok.type = TOKEN_DOT; strcpy(tok.value, "."); return tok; }
+        if (c == '^') { tok.type = TOKEN_CARET; strcpy(tok.value, "^"); return tok; }
         if (c == '+') { tok.type = TOKEN_PLUS; strcpy(tok.value, "+"); return tok; }
-        if (c == '-') { tok.type = TOKEN_MINUS; strcpy(tok.value, "-"); return tok; }
         if (c == '*') { tok.type = TOKEN_STAR; strcpy(tok.value, "*"); return tok; }
 
-        /* Single/Double Slash & Line Comments */
+        /* 6. Multi-character Operators */
+        if (c == '-') {
+            int next_c = fgetc(file);
+            if (next_c == '>') {
+                tok.type = TOKEN_ARROW;
+                strcpy(tok.value, "->");
+            } else {
+                ungetc(next_c, file);
+                tok.type = TOKEN_MINUS;
+                strcpy(tok.value, "-");
+            }
+            return tok;
+        }
+
         if (c == '/') {
             int next_c = fgetc(file);
             if (next_c == '/') {
@@ -156,15 +176,49 @@ Token next_token(FILE *file) {
             return tok;
         }
 
+        if (c == '<') {
+            int next_c = fgetc(file);
+            if (next_c == '=') {
+                tok.type = TOKEN_LTE;
+                strcpy(tok.value, "<=");
+            } else if (next_c == '<') {
+                tok.type = TOKEN_SHL;
+                strcpy(tok.value, "<<");
+            } else {
+                ungetc(next_c, file);
+                tok.type = TOKEN_LT;
+                strcpy(tok.value, "<");
+            }
+            return tok;
+        }
+
+        if (c == '>') {
+            int next_c = fgetc(file);
+            if (next_c == '=') {
+                tok.type = TOKEN_GTE;
+                strcpy(tok.value, ">=");
+            } else if (next_c == '>') {
+                tok.type = TOKEN_SHR;
+                strcpy(tok.value, ">>");
+            } else {
+                ungetc(next_c, file);
+                tok.type = TOKEN_GT;
+                strcpy(tok.value, ">");
+            }
+            return tok;
+        }
+
         if (c == '&') {
             int next_c = fgetc(file);
             if (next_c == '&') {
                 tok.type = TOKEN_AND;
                 strcpy(tok.value, "&&");
-                return tok;
             } else {
                 ungetc(next_c, file);
+                tok.type = TOKEN_AMP;
+                strcpy(tok.value, "&");
             }
+            return tok;
         }
 
         if (c == '|') {
@@ -172,36 +226,15 @@ Token next_token(FILE *file) {
             if (next_c == '|') {
                 tok.type = TOKEN_OR;
                 strcpy(tok.value, "||");
-                return tok;
             } else {
                 ungetc(next_c, file);
+                tok.type = TOKEN_PIPE;
+                strcpy(tok.value, "|");
             }
+            return tok;
         }
     }
-    if (ch == '@') {
-    char buffer[32] = {0};
-    int idx = 0;
-    
-    while (isalnum(peek_next_char(lexer)) || peek_next_char(lexer) == '.') {
-        buffer[idx++] = get_next_char(lexer);
-        if (idx >= sizeof(buffer) - 1) break;
-    }
 
-    if (strcmp(buffer, "exit") == 0) return create_token(TOKEN_AT_EXIT, "@exit");
-    if (strcmp(buffer, "panic") == 0) return create_token(TOKEN_AT_PANIC, "@panic");}
-    if (ch == '<') {
-    if (peek_next_char(lexer) == '=') {
-        get_next_char(lexer);
-        return create_token(TOKEN_LTE, "<=");
-    }
-    return create_token(TOKEN_LT, "<");}
-    if (ch == '>') {
-    if (peek_next_char(lexer) == '=') {
-        get_next_char(lexer);
-        return create_token(TOKEN_GTE, ">=");
-    }
-    return create_token(TOKEN_GT, ">");}
-    
     tok.line = current_line;
     tok.type = TOKEN_EOF;
     tok.value[0] = '\0';
